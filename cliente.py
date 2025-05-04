5f8decb2bb024f8ad33bab3ba887b99a59852b3b
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-05-04 13:20:42 -0300

    metodo str classe cliente

--- cliente.py
+++ cliente.py
@@ -1,12 +1,15 @@
 class Cliente:
 
-    lista_clientes = []
+    lista_de_clientes = []
 
     def __init__(self, nome, cpf, email):
         self.nome = nome
         self.cpf = cpf
         self.email = email
 
-        Cliente.lista_clientes.append(
+        Cliente.lista_de_clientes.append(
             {"nome": self.nome, "cpf": self.cpf, "email": self.email}
         )
+
+    def __str__(self):
+        return f"NOME: {str.upper(self.nome)}\nE-MAIL: {self.email}"
