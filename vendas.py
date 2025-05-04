17afdb83dc7bcde062aebcced52a40342ea3fb4a
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-05-04 13:42:23 -0300

    sistema de id de vendas e metodo str

--- vendas.py
+++ vendas.py
@@ -1,8 +1,12 @@
 class Venda:
 
     lista_de_vendas = []
+    id_venda = 1
 
     def __init__(self, cliente, produtos, quantidade):
+        self.id_venda = Venda.id_venda
+        Venda.id_venda += 1
+
         self.cliente = cliente
         self.produtos = produtos
         self.quantidade = quantidade
@@ -14,3 +18,6 @@ class Venda:
                 "quantidade": self.quantidade,
             }
         )
+
+    def __str__(self):
+        return f"ID_VENDA: {self.id_venda}\nCLIENTE: {str.upper(self.cliente)}\nPRODUTOS: {str.upper(self.produtos)}\nQUANTIDADE: {self.quantidade}"
